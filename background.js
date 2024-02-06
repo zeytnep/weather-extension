ff27d7c2014f37a078ad8989f99d671e43e32853
Zeynep Erdogru <46981050+zeytnep@users.noreply.github.com>
2024-02-06 13:37:21 -0500

    added functionality to see other cities weather data

--- background.js
+++ background.js
@@ -1,22 +1,25 @@
 // background.js
+// Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
+const apiKey = YOUR_API_KEY;
+console.log("here 1");
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log("here 2");
     if (request.action === "getWeather") {
+      console.log("displaying fetchWeatherData(); if statement");
       fetchWeatherData();
+    } 
+    if (request.action === "getWeather" && typeof request.city == "string") {
+      console.log("displaying fetchWeatherDataForCity() if statement");
+      fetchWeatherDataForCity(request.city);
     }
   });
   
   function fetchWeatherData() {
-    // Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
-    const apiKey = '65892d7ba089035d1bfe7fe7a045ae4b';
+    
     const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=GUELPH&appid=' + apiKey;
   
     fetch(apiUrl)
-      .then(response => {
-        
-        //log the raw API response 
-        console.log('API Response: ', response);
-        return response.json()
-      })
+      .then(response => { return response.json() })
       .then(data => {
         const tempatureKelvin = data.main.temp;
         const temperatureCelsius = tempatureKelvin - 273.15;
@@ -26,5 +29,21 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       .catch(error => {
         console.error('Error fetching weather data:', error);
       });
+  }
+
+  function fetchWeatherDataForCity(city) {
+    
+    const apiUrl2 = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+
+    fetch(apiUrl2) 
+    .then(response => { return response.json() })
+    .then(data => {
+      const tempatureKelvin = data.main.temp;
+      const temperatureCelsius = tempatureKelvin - 273.15;
+      const weatherInfo = `Temperature: ${Math.round(temperatureCelsius)}°C, ${data.weather[0].description}, Humidity: ${data.main.humidity} %`;
+      chrome.runtime.sendMessage({ action: "updateWeather", weatherInfo: weatherInfo });
+    })
+
+  
   }
   
--- popup.js
+++ popup.js
@@ -1,6 +1,18 @@
 // popup.js
 document.getElementById("getWeather").addEventListener("click", function() {
-    chrome.runtime.sendMessage({ action: "getWeather" });
+    const cityInput = document.getElementById("cityInput");
+    const cityString = cityInput.value;
+
+
+    console.log("Displaying city.trim()");
+    console.log(cityString.trim());
+
+    if (cityString.trim()) {
+      chrome.runtime.sendMessage({ action: "getWeather", city: cityString });
+    } else {
+      alert("Please enter a city name. ")
+    }
+    //chrome.runtime.sendMessage({ action: "getWeather" });
   });
   
   chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
